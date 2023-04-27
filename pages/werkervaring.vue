bb45b9063a35df4fc7692b2efe53b3a5db19ea00
Evi <mail@eviwammes.nl>
2023-04-27 14:05:40 +0200

    error oplossen

--- pages/werkervaring.vue
+++ pages/werkervaring.vue
@@ -9,17 +9,6 @@
       Mijn hobby's zijn onder andere skateboarden 🛹, het luisteren van muziek 🎵, en het leren van nieuwe dingen. <br><br> Om verder te experimenteren met de genoemde programmeertalen is deze portfolio ook gebouwd met VueJS, en Nuxt3, binnenkort zullen er ook 3d elementen in komen met ThreeJS 😉
       </p>
     </div>
-    <div class="w-full justify-center hidden sm:flex">
-      <div class="flex flex-col items-center dark:shadow-[0_20px_25px_-5px_rgba(255,255,255,0.3)] shadow-xl rounded-3xl 2xl:w-[60%] w-[60%] hover:shadow-2xl dark:hover:shadow-[0_20px_50px_-5px_rgba(255,255,255,0.3)]">
-        <img src="~/assets/foto_evi.png" alt="Foto van Evi Wammes"/>
-        <ul class="mt-5 bg-black text-white rounded p-3 w-full -mt-1">
-          <li>🥳 19</li>
-          <li><a href="mailto:mail@eviwammes.nl">✉️ mail@eviwammes.nl</a></li>
-          <li><a href="tel:+31640707077">📞 +31640707077</a></li>
-          <li>📍Amsterdam</li>
-        </ul>
-      </div>
-    </div>
   </div>
 
 </template>
