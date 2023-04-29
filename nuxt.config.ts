c72b34ebc3acca618b837e9a4cbadd72ba7ff525
Evi <mail@eviwammes.nl>
2023-04-29 17:03:21 +0200

    Meta-beschrijving toegevoegd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    pageTransition: {name: 'page', mode: 'out-in'}
+    pageTransition: {name: 'page', mode: 'out-in'},
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1'
+    }
   },
     postcss: {
         plugins: {
