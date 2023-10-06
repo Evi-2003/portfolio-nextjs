48d682346a8dac5786201bc949d351e3dc6959ae
Evi <69012052+Evi-2003@users.noreply.github.com>
2023-10-06 16:38:38 +0200

    fix robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,24 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
+import robotsTxt from 'astro-robots-txt';
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(),     robotsTxt({
+    host: 'eviwammes.amsterdam',
+    policy: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/projecten',
+      },
+    ]
+  }),],
   output: "hybrid",
   adapter: cloudflare(),
   image: {
     service: { entrypoint: 'astro/assets/services/sharp' },
   },
+
 });
