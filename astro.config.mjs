be6585afb821472e5cdd461b771eae84de701bf7
Evi <69012052+Evi-2003@users.noreply.github.com>
2023-10-26 23:35:13 +0200

    Over mij + Projecten

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import robotsTxt from 'astro-robots-txt';
+
 import cloudflare from "@astrojs/cloudflare";
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://eviwammes.amsterdam',
-  integrations: [tailwind(), sitemap(), robotsTxt({
-    host: 'eviwammes.amsterdam',
-    policy: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: '/projecten',
-      },
-    ]
-  }),],
+  integrations: [tailwind()],
   output: "hybrid",
   adapter: cloudflare(),
   image: {
     service: { entrypoint: 'astro/assets/services/sharp' },
   },
-
 });
