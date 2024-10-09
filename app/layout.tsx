c931cd3d35e5ae719a9e30062013ea5c2459e9fe
Evi <mail@eviwammes.nl>
2024-10-09 13:02:19 +0200

    feat(layout): added plausible data collection

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 
 import './globals.css'
 import Header from './components/Header'
+import PlausibleProvider from 'next-plausible'
 
 export default function RootLayout({
   children,
@@ -9,11 +10,13 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="nl-NL">
-      <body className={'flex flex-col items-center justify-center bg-indigo-100 dark:bg-neutral-900 pb-5 '}>
-        <Header />
-        {children}
-      </body>
-    </html>
+    <PlausibleProvider domain="eviwammes.dev">
+      <html lang="nl-NL">
+        <body className={'flex flex-col items-center justify-center bg-indigo-100 dark:bg-neutral-900 pb-5 '}>
+          <Header />
+          {children}
+        </body>
+      </html>
+    </PlausibleProvider>
   )
 }
