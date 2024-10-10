bb7ae9a9b63b66ce6cf8fd11ae4ffec5a450f909
Evi <mail@eviwammes.nl>
2024-10-10 22:45:18 +0200

    fix(fonts): switched to Geist

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import Header from './components/Header'
 import PlausibleProvider from 'next-plausible'
 import NavProvider from './components/providers/NavProvider'
 
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -14,7 +17,7 @@ export default function RootLayout({
     <PlausibleProvider domain="eviwammes.dev">
       <NavProvider>
         <html lang="nl-NL">
-          <body className={'flex flex-col items-center from-white to-gray-100 bg-gradient-to-b min-h-screen dark:from-neutral-900 dark:to-neutral-950 pb-5 '}>
+          <body className={`flex flex-col items-center from-white to-gray-100 bg-gradient-to-b min-h-screen dark:from-neutral-900 dark:to-neutral-950 pb-5 ${GeistSans.className}`}>
             <Header />
             {children}
           </body>
