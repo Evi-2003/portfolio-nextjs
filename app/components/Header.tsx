e42bd8c82d8582a90f9b53933b5a9239d471ed75
Evi <mail@eviwammes.nl>
2024-10-20 19:07:06 +0200

    Revert "fix(NavItem): prefetching"
    This reverts commit 351d192bb32f6cfffa80a04d86938b3b50acc21c.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -39,8 +39,8 @@ const Header = async ({ lng }: { lng: string }) => {
     <header className={'my-10 flex w-full items-center justify-center ' + zenKurenaido.className}>
       <Head>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="https://www.eviwammes.dev/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="https://www.eviwammes.dev/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
