cc011bf795140a5be4f2bd657c2565d9f5864de8
Evi <mail@eviwammes.nl>
2024-10-27 11:02:19 +0100

    fix(layout): added favicon

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -18,6 +18,12 @@ export default async function RootLayout(
     <PlausibleProvider domain="eviwammes.dev">
       <NavProvider>
         <html lang="nl-NL">
+          <head>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+          </head>
           <body
             className={`flex min-h-screen flex-col items-center bg-gradient-to-b from-white to-gray-100 pb-5
               dark:from-neutral-900 dark:to-neutral-950 ${GeistSans.className}`}
