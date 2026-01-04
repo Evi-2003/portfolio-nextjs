b7155ab29228bf09d983fa2915d1a669b44857e7
Wammes <mail@eviwammes.nl>
2026-01-04 16:36:40 +0100

    PR Feedback

--- app/components/NavItem.tsx
+++ app/components/NavItem.tsx
@@ -1,12 +1,17 @@
-'use client'
+'use client';
+
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useContext } from 'react';
+import { NavContext } from './providers/NavProvider';
 
 const NavItem = ({ label, slug, idx }: { label: string; slug: string; idx: number }) => {
   const pathname = usePathname();
+  const { toggleIsOpen } = useContext(NavContext);
 
   return (
     <li
+      onClick={() => toggleIsOpen()}
       className={
         pathname === slug || (pathname === '/' && idx === 0) ? 'rounded-lg bg-stone-300 p-2 dark:bg-stone-900' : 'p-2'
       }
